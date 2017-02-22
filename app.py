85f2e4023eedc04cb620e821b6c194613931fa6f
Andrew Norton <hskrus@gmail.com>
2017-02-21 23:35:01 -0500

    Add hello world response for *every* message that is sent

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import os
 import sys
 import json
 
+from urllib.parse import urlencode
+from urllib.reqest import Request, urlopen
+
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -11,6 +14,20 @@ app = Flask(__name__)
 def webhook():
   data = request.get_json()
   log(data)
+
+  send_message('Hello, world!')
+
+  return "ok", 200
+
+def send_message(msg):
+  url  = 'https://api.groupme.com/v3/bots/post'
+  data = {
+          'bot_id' : '5b69eeb4c800c919c2aff81edc',
+          'text'   : msg,
+         }
+  request = Request(url, urlencode(data).encode())
+  json = urlopen(request).read().decode()
+  log(json)
   
 def log(msg):
   print(str(msg))
